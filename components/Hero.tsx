cd2ebeacf8b830d6f0465ed011e46a95f5d7652e
Mihir Thanekar <mthaneka@uci.edu>
2021-06-12 22:43:47 -0700

    Reuse SpacedGroup style in Hero.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,33 +1,35 @@
-import Image from 'next/image'
-import styled from 'styled-components'
-import { CenteredFlex, Title, SecondaryTitle } from '../styles/global-styled'
-import SocialIcons from './SocialIcons'
+import Image from "next/image";
+import styled from "styled-components";
+import {
+  CenteredFlex,
+  Title,
+  SecondaryTitle,
+  SpacedGroup,
+} from "../styles/global-styled";
+import SocialIcons from "./SocialIcons";
 
 export default function Hero() {
   return (
     <CenteredFlex column={true}>
-      <SpacedGroup vertical="3rem">
+      <CenteredSpacedGroup vertical="3rem">
         <Title>Mihir Thanekar</Title>
         <SecondaryTitle>Student Software Developer</SecondaryTitle>
-      </SpacedGroup>
+      </CenteredSpacedGroup>
       <Image
         src="/hero_image.png"
         alt="Cartoon Mihir with a laptop."
         width={500}
         height={500}
       />
-      <SpacedGroup vertical="2rem">
-        <SocialIcons/>
-      </SpacedGroup>
+      <CenteredSpacedGroup vertical="2rem">
+        <SocialIcons />
+      </CenteredSpacedGroup>
     </CenteredFlex>
   );
-};
+}
 
-const SpacedGroup = styled.div`
-  margin: ${(props: {vertical: string}) => props.vertical} 0;
-
+const CenteredSpacedGroup = styled(SpacedGroup)`
   > * {
-    margin: 0;
     text-align: center;
   }
 `;
