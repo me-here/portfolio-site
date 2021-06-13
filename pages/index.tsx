d94b61ab86c0a0bd249742aa8f9715a90dd8a459
Mihir Thanekar <mthaneka@uci.edu>
2021-06-13 14:13:29 -0700

    Temporarily added site verification meta tag.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,39 @@
-import Head from 'next/head'
-import Hero from '../components/Hero'
-import Navbar from '../components/Navbar'
-import IntroSection from '../components/IntroSection'
-import SkillsSection from '../components/SkillsSection'
-import WorkSection from '../components/WorkSection'
-import Footer from '../components/Footer'
+import Head from "next/head";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import IntroSection from "../components/IntroSection";
+import SkillsSection from "../components/SkillsSection";
+import WorkSection from "../components/WorkSection";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Mihir Thanekar -- Developer</title>
-        <meta name="description" content="Mihir Thanekar's Developer Portfolio" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
+        <meta
+          name="description"
+          content="Mihir Thanekar's Developer Portfolio"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+        />
+        <meta
+          name="google-site-verification"
+          content="yz5DT9GPSxi3NY0e_IDVgnfuH2wS8e_gJUWOxNKIXM4"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Navbar/>
+      <Navbar />
       <main>
-        <Hero/>
-        <IntroSection/>
-        <SkillsSection/>
-        <WorkSection/>
+        <Hero />
+        <IntroSection />
+        <SkillsSection />
+        <WorkSection />
       </main>
       <Footer />
     </div>
-  )
+  );
 }
